9062880445237e97778446b9467eaf9452cf06b3
Ahmet Onder Mogol <aomogol@gmail.com>
2021-11-04 17:54:41 +0300

    15 -5

--- 0-preinstall.sh
+++ 0-preinstall.sh
@@ -33,7 +33,7 @@ echo -e "-Setting up $iso mirrors for faster downloads"
 echo -e "-------------------------------------------------------------------------"
 #reflector -a 48 -c $iso -f 5 -l 20 --sort rate --save /etc/pacman.d/mirrorlist
 
-reflector -a 48 -f 15 -l 20 --sort rate --save /etc/pacman.d/mirrorlist
+reflector -a 48 -f 5 -l 20 --sort rate --save /etc/pacman.d/mirrorlist
 mkdir /mnt
 
 
